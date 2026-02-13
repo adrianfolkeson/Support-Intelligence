f115a02847ddabc3b6fa6d999b746a5ac3c41255
Adrian Folkeson <adrianfolkeson@MacBook-Air-som-tillhor-Adrian.local>
2026-02-13 14:10:57 +0100

    Fix Clerk middleware with publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)", "/api/webhook(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in((?!.*)", "/sign-up((?!.*)", "/api/webhook((?!.*)"]);
 
-export default clerkMiddleware((req) => {
-  if (isPublicRoute(req)) {
-    return;
-  }
+export default clerkMiddleware({
+  publicRoutes: isPublicRoute,
+  debug: false,
 });
-
-export const config = {
-  matcher: ["/((?!_next|favicon|static).*)"],
-};
